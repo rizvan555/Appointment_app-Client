6df4ced3b3f7ad7268bced592a31676b3e26e746
Rizvan Karimov <karimovrizvan84@gmail.com>
2024-02-15 09:25:45 +0100

    used cypress testing with to navigate to another page,testin about length and tags

--- cypress/e2e/homePageTest.cy.ts
+++ cypress/e2e/homePageTest.cy.ts
@@ -1,22 +1,29 @@
 describe('Test_click_to_service', () => {
   beforeEach(() => {
-      cy.visit('http://localhost:5173/');
+    cy.visit('http://localhost:5173/');
   });
 
-  it('check_url', () => {
+  it('check_homePage', () => {
     cy.url().should('eq', 'http://localhost:5173/');
     cy.url().should('contain', 'localhost');
     cy.url().should('include', '5173');
-  });
 
-  it('check_location', () => {
     cy.location('pathname').should('eq', '/');
-  });
-});
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  if (err.message.includes('Script error')) {
-    return false;
-  }
-  return true;
+    cy.get('.home-bg aside h1')
+      .should('contain', 'Wilkommen beim Barbershop')
+      .should('be.visible');
+    cy.get('.home-bg aside h1').should('have.length', 1);
+    cy.get('.home-bg aside h1').its('length').and('eq', 1);
+    cy.get('.home-bg button a').should('have.attr', 'href', '/about');
+
+
+  });
+
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    if (err.message.includes('Script error')) {
+      return false;
+    }
+    return true;
+  });
 });
