011e78a1880e4059d4fd4883d468e0e63d92cd85
Rizvan Karimov <karimovrizvan84@gmail.com>
2024-02-15 09:49:22 +0100

    used back,forward or -1&1

--- cypress/e2e/homePageTest.cy.ts
+++ cypress/e2e/homePageTest.cy.ts
@@ -1,6 +1,6 @@
 describe('Test_click_to_service', () => {
   beforeEach(() => {
-    cy.viewport(1200, 1000);
+    cy.viewport(1200, 100);
     cy.visit('http://localhost:5173/');
   });
 
@@ -16,10 +16,25 @@ describe('Test_click_to_service', () => {
       .should('be.visible');
     cy.get('.home-bg aside h1').should('have.length', 1);
     cy.get('.home-bg aside h1').its('length').and('eq', 1);
+
+    //buttona basdiqda "about" sehifesine kec
     cy.get('.home-bg button a').should('have.attr', 'href', '/about');
 
-
+    //buttona basdiqda "about" sehifesine kec,2 saniye gözle ve geri qayit ve sonra yeniden about sehifesine kec
+    cy.get('.home-bg button a').click();
+    cy.url().should('eq', 'http://localhost:5173/about');
+    cy.wait(2000);
+    cy.go('back');
+    cy.wait(2000);
+    cy.go('forward');
+    //***********************************************************/
+    cy.wait(2000);
+    cy.go(-1); //"back"-i belece -1 ile de yaza bilerik
+    cy.wait(2000);
+    cy.go(1); //"forward"-i belece 1 ile de yaza bilerik
   });
+
+  
 
   Cypress.on('uncaught:exception', (err, runnable) => {
     if (err.message.includes('Script error')) {
