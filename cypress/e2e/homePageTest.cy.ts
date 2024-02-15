16eba76a2418d57c29ec918f8797439792a4847d
Rizvan Karimov <karimovrizvan84@gmail.com>
2024-02-15 10:31:48 +0100

    used POM for homepage and imported to test to test file

--- cypress/e2e/homePageTest.cy.ts
+++ cypress/e2e/homePageTest.cy.ts
@@ -1,45 +1,18 @@
+import { HomePage } from '../../cypress/homePageObjectModel/homepage';
+const homePagePom = new HomePage();
+
 describe('Test_click_to_service', () => {
   beforeEach(() => {
-    cy.viewport(1200, 100);
-    cy.visit('http://localhost:5173/');
+    homePagePom.navigate();
   });
 
   it('check_homePage', () => {
-    cy.url().should('eq', 'http://localhost:5173/');
-    cy.url().should('contain', 'localhost');
-    cy.url().should('include', '5173');
-
-    cy.location('pathname').should('eq', '/');
-
-    cy.get('.home-bg aside h1')
-      .should('contain', 'Wilkommen beim Barbershop')
-      .should('be.visible');
-    cy.get('.home-bg aside h1').should('have.length', 1);
-    cy.get('.home-bg aside h1').its('length').and('eq', 1);
-
-    //buttona basdiqda "about" sehifesine kec
-    cy.get('.home-bg button a').should('have.attr', 'href', '/about');
-
-    //buttona basdiqda "about" sehifesine kec,2 saniye gözle ve geri qayit ve sonra yeniden about sehifesine kec
-    cy.get('.home-bg button a').click();
-    cy.url().should('eq', 'http://localhost:5173/about');
-    cy.wait(2000);
-    cy.go('back');
-    cy.wait(2000);
-    cy.go('forward');
-    //***********************************************************/
-    cy.wait(2000);
-    cy.go(-1); //"back"-i belece -1 ile de yaza bilerik
-    cy.wait(2000);
-    cy.go(1); //"forward"-i belece 1 ile de yaza bilerik
-  });
+    homePagePom.url();
+    homePagePom.location();
+    homePagePom.visible();
+    homePagePom.href();
+    homePagePom.forward_back();
 
   
-
-  Cypress.on('uncaught:exception', (err, runnable) => {
-    if (err.message.includes('Script error')) {
-      return false;
-    }
-    return true;
   });
 });
---
+++ cypress/homePageObjectModel/homepage.ts
@@ -0,0 +1,53 @@
+export class HomePage {
+  navigate() {
+    cy.viewport(1200, 100);
+    cy.visit('http://localhost:5173/');
+  }
+
+  url() {
+    cy.url().should('eq', 'http://localhost:5173/');
+    cy.url().should('contain', 'localhost');
+    cy.url().should('include', '5173');
+  }
+
+  location() {
+    cy.location('pathname').should('eq', '/');
+  }
+
+  visible() {
+    cy.get('.home-bg aside h1')
+      .should('contain', 'Wilkommen beim Barbershop')
+      .should('be.visible');
+    cy.get('.home-bg aside h1').should('have.length', 1);
+    cy.get('.home-bg aside h1').its('length').and('eq', 1);
+  }
+
+  href() {
+    //buttona basdiqda "about" sehifesine kec
+    cy.get('.home-bg button a').should('have.attr', 'href', '/about');
+  }
+
+  forward_back() {
+    //buttona basdiqda "about" sehifesine kec,2 saniye gözle ve geri qayit ve sonra yeniden about sehifesine kec
+    cy.get('.home-bg button a').click();
+    cy.url().should('eq', 'http://localhost:5173/about');
+    cy.wait(2000);
+    cy.go('back');
+    cy.wait(2000);
+    cy.go('forward');
+    //***********************************************************/
+    cy.wait(2000);
+    cy.go(-1); //"back"-i belece -1 ile de yaza bilerik
+    cy.wait(2000);
+    cy.go(1); //"forward"-i belece 1 ile de yaza bilerik
+    cy.wait(2000);
+    cy.go(-1); //"back"-i belece -1 ile de yaza bilerik
+  }
+}
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  if (err.message.includes('Script error')) {
+    return false;
+  }
+  return true;
+});
