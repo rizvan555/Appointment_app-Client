7ecb80104eae6ef116212d12df8e80e11f8b1c47
Rizvan Karimov <karimovrizvan84@gmail.com>
2024-02-20 11:40:34 +0100

    created testing for login page with playwright testing

--- tests/home.test.ts
+++ tests/home.test.ts
@@ -2,20 +2,16 @@ import { expect, test } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
-  await page.getByRole('button', { name: 'Anmelden' }).click();
-  await page.getByRole('link', { name: 'Login' }).click();
-  await page.getByPlaceholder('Username').click();
-  await expect(page.getByPlaceholder('Username')).toHaveCount(1);
-  await page.getByPlaceholder('Username').fill('rizvan');
-  await page.getByPlaceholder('Password').click();
-  await expect(page.getByPlaceholder('Password')).toHaveCount(1);
-  await page.getByPlaceholder('Password').fill('111111');
-  await page.getByPlaceholder('Password').press('Enter');
+  await page.waitForTimeout(1000);
+
   await page.getByRole('link', { name: 'Home' }).click();
+  await page.waitForTimeout(1000);
   await page.getByRole('link', { name: 'Team' }).click();
+  await page.waitForTimeout(1000);
   await page.getByRole('link', { name: 'Termin' }).click();
+  await page.waitForTimeout(1000);
   await page.getByRole('link', { name: 'Home' }).click();
-  
+
   await expect(
     page.getByRole('heading', { name: 'Wir machen Ihren Stil zu' })
   ).toHaveText('Wir machen Ihren Stil zu Ihren Träumen');
---
+++ tests/login.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@playwright/test';
+
+test('test_login', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByRole('button', { name: 'Anmelden' })).toBeVisible();
+  await page.getByRole('button', { name: 'Anmelden' }).click();
+
+  await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
+  await page.waitForTimeout(1000);
+  await page.getByRole('link', { name: 'Login' }).click();
+
+  await page.goto('http://localhost:5173/login');
+  await page.waitForTimeout(1000);
+  await expect(page.getByPlaceholder('Username')).toBeVisible();
+  await page.getByPlaceholder('Username').click();
+  await expect(page.getByPlaceholder('Username')).toHaveCount(1);
+  await page.getByPlaceholder('Username').fill('rizvan');
+  await page.waitForTimeout(1000);
+
+  await expect(page.getByPlaceholder('Password')).toBeVisible();
+  await page.getByPlaceholder('Password').click();
+  await expect(page.getByPlaceholder('Password')).toHaveCount(1);
+  await page.getByPlaceholder('Password').fill('111111');
+  await page.waitForTimeout(1000);
+
+  await expect(page.getByRole('button', { name: 'LOGIN' })).toBeVisible();
+  await page.getByRole('button', { name: 'LOGIN' }).click();
+  await page.waitForTimeout(1000);
+});
