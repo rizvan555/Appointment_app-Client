9bab34867aa64aa7a091fce8a9fdd33163262fa7
Rizvan Karimov <karimovrizvan84@gmail.com>
2024-02-21 14:13:00 +0100

    added testing for service5,6,7,8,9

--- tests/services/service9.test.ts
+++ tests/services/service9.test.ts
@@ -1,3 +1,49 @@
-import {test,expect} from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-test('test_service9', async ({ page }) => {});
+test('test_service9', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByRole('button', { name: 'Anmelden' })).toBeVisible();
+  await page.getByRole('button', { name: 'Anmelden' }).click();
+  await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.goto('http://localhost:5173/login');
+  await expect(page.getByPlaceholder('Username')).toBeVisible();
+  await page.getByPlaceholder('Username').click();
+  await expect(page.getByPlaceholder('Username')).toHaveCount(1);
+  await page.getByPlaceholder('Username').fill('rizvan');
+  await expect(page.getByPlaceholder('Password')).toBeVisible();
+  await page.getByPlaceholder('Password').click();
+  await expect(page.getByPlaceholder('Password')).toHaveCount(1);
+  await page.getByPlaceholder('Password').fill('111111');
+  await expect(page.getByRole('button', { name: 'LOGIN' })).toBeVisible();
+  await page.getByRole('button', { name: 'LOGIN' }).click();
+  await page.waitForTimeout(1000);
+  await page.goto('http://localhost:5173/');
+  await page.waitForTimeout(1000);
+  await expect(page.getByRole('link', { name: 'Termin' })).toBeVisible();
+  await page.getByRole('link', { name: 'Termin' }).click();
+  await expect(page.url()).toBe('http://localhost:5173/termin');
+  await page.waitForTimeout(1000);
+  await expect(
+    page.getByRole('heading', { name: 'Unsere Angebote' })
+  ).toBeVisible();
+  await expect(
+    page.getByRole('link', { name: 'haarImage Nassrasur Model /' })
+  ).toBeVisible();
+  await page.getByRole('link', { name: 'haarImage Nassrasur Model /' }).click();
+  await page.waitForTimeout(1000);
+  await expect(page.url()).toBe('http://localhost:5173/service9');
+  await page.waitForTimeout(1000);
+
+  await expect(page.locator('.vc-pane')).toBeVisible();
+  await expect(page.locator('div .vc-header').first()).toBeVisible();
+  await expect(page.locator('div .vc-weeks').first()).toBeVisible();
+  await expect(page.locator('div .vc-weekdays').first()).toBeVisible();
+  await expect(page.locator('div .vc-weekday').first()).toBeVisible();
+  await expect(page.locator('div .vc-week').first()).toBeVisible();
+  await expect(page.locator('div .vc-day').first()).toBeVisible();
+  await expect(page.locator('div .vc-day-content').first()).toBeVisible();
+  await page.getByLabel('Monday, Feb 26,').click();
+  await page.waitForTimeout(1000);
+  await page.getByRole('button', { name: ':00 am - 08:30 am' }).click();
+});
